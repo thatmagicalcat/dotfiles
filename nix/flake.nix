04cf4c187ed412c8f611ca18d14e858d6d5ab085
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-01-05 21:29:20 +0530

    idk what's changed

---
+++ nix/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake/beta";
+    nil.url = "github:oxalica/nil";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.DELTA = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          {
+            config._module.args = {
+              inherit inputs;
+            };
+          }
+        ];
+      };
+    };
+}
