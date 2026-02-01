6a50b9e194cf9ebe038442fcf4e22229d3b44362
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-02-01 15:47:18 +0530

    add gitignore

--- river/target/debug/build/anyhow-e1830ac0a1147c61/build_script_build-e1830ac0a1147c61.d
+++
@@ -1,5 +0,0 @@
-/home/thatmagicalcat/dotfiles/river/target/debug/build/anyhow-e1830ac0a1147c61/build_script_build-e1830ac0a1147c61.d: /home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs
-
-/home/thatmagicalcat/dotfiles/river/target/debug/build/anyhow-e1830ac0a1147c61/build_script_build-e1830ac0a1147c61: /home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs
-
-/home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs:
--- river/target/release/build/anyhow-e38c5c962c436c37/build_script_build-e38c5c962c436c37.d
+++
@@ -1,5 +0,0 @@
-/home/thatmagicalcat/dotfiles/river/target/release/build/anyhow-e38c5c962c436c37/build_script_build-e38c5c962c436c37.d: /home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs
-
-/home/thatmagicalcat/dotfiles/river/target/release/build/anyhow-e38c5c962c436c37/build_script_build-e38c5c962c436c37: /home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs
-
-/home/thatmagicalcat/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anyhow-1.0.100/build.rs:
