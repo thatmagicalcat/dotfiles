2eaa9b3ed217d07aa8e09180524e589068d8a694
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-02-11 19:23:00 +0530

    added window rules

--- river/src/rules.rs
+++ river/src/rules.rs
@@ -2,6 +2,9 @@ use crate::riverctl::riverctl;
 use anyhow::Result;
 
 pub fn run() -> Result<()> {
-    riverctl(&["rule-add", "-app-id", "zen", "tags", "1"])?;
+    riverctl(&["rule-add", "-app-id", "zen-beta", "tags", "1"])?;
+    riverctl(&["rule-add", "-app-id", "zen-beta", "csd"])?;
+    riverctl(&["rule-add", "-app-id", "Spotify", "tags", &(1 << 4).to_string()])?;
+    riverctl(&["rule-add", "-app-id", "vesktop", "tags", &(1 << 3).to_string()])?;
     Ok(())
 }
