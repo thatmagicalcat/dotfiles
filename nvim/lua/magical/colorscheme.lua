7640b2c2091c090cf6d0df6e6428b559d8d4c19c
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-02-25 20:53:47 +0530

    zenbones theme

--- nvim/lua/magical/colorscheme.lua
+++ nvim/lua/magical/colorscheme.lua
@@ -1,12 +1,12 @@
-local colorscheme = "gruvbox"
+-- local colorscheme = "gruvbox"
 -- local colorscheme = "monokai_pro"
 
-local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not is_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
-end
+-- local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not is_ok then
+--     vim.notify("colorscheme " .. colorscheme .. " not found!")
+--     return
+-- end
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "#101010" })
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
