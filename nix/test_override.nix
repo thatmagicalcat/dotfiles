ff1c938453f088d891ccbb8b87c093e12d440e2a
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-03-21 23:42:38 +0530

    test(nix): add nix override test files

---
+++ nix/test_override.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.waydroid.overrideAttrs (old: {
+  postPatch = (old.postPatch or "") + ''
+    sed -i 's/LXC_USE_NFT="false"/LXC_USE_NFT="true"/' data/scripts/waydroid-net.sh
+  '';
+  preFixup = (old.preFixup or "") + ''
+    wrapProgram $out/lib/waydroid/data/scripts/waydroid-net.sh \
+      --prefix PATH ":" ${pkgs.lib.makeBinPath [ pkgs.nftables ]}
+  '';
+})
